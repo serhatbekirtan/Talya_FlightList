c007b56af4e8889f3a134653d05a30620baf68e9
Serhat Bekir Tan <serhatbekirtan@gmail.com>
2023-07-06 01:25:48 +0300

    add html css js json files.

---
+++ custom.js
@@ -0,0 +1,289 @@
+response = {
+    "response": [
+        {
+            "AIRPORTCODE": "LON",
+            "AIRPORTNAME": "Londra",
+            "CITYNAME": "Londra",
+            "CITYCODE": "LON",
+            "COUNTRYNAME": "Birleşik Krallık",
+            "COUNTRYCODE": "GB",
+            "LOCATIONTYPE": "C",
+            "ORDERID": 1,
+            "RANK": 0,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "ELS",
+            "AIRPORTNAME": "East London (ELS)",
+            "CITYNAME": "East London",
+            "CITYCODE": "ELS",
+            "COUNTRYNAME": "Güney Afrika",
+            "COUNTRYCODE": "ZA",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "GGG",
+            "AIRPORTNAME": "Longview, Teksas (GGG-Doğu Teksas Bölge)",
+            "CITYNAME": "Longview",
+            "CITYCODE": "GGG",
+            "COUNTRYNAME": "Birleşik Devletler",
+            "COUNTRYCODE": "US",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "GON",
+            "AIRPORTNAME": "New London, Connecticut (GON-Groton - New London)",
+            "CITYNAME": "New London",
+            "CITYCODE": "GON",
+            "COUNTRYNAME": "Birleşik Devletler",
+            "COUNTRYCODE": "US",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "GSA",
+            "AIRPORTNAME": "Long Pasia (GSA)",
+            "CITYNAME": "Long Pasia",
+            "CITYCODE": "GSA",
+            "COUNTRYNAME": "Malezya",
+            "COUNTRYCODE": "MY",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "HAP",
+            "AIRPORTNAME": "Long Island, Queensland (HAP)",
+            "CITYNAME": "Long Island",
+            "CITYCODE": "HAP",
+            "COUNTRYNAME": "Avustralya",
+            "COUNTRYCODE": "AU",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "KLS",
+            "AIRPORTNAME": "Kelso Longview Regional Havali (KLS)",
+            "CITYNAME": "Kelso",
+            "CITYCODE": "KLS",
+            "COUNTRYNAME": "Birleşik Devletler",
+            "COUNTRYCODE": "US",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LBP",
+            "AIRPORTNAME": "Long Banga (LBP)",
+            "CITYNAME": "Long Banga",
+            "CITYCODE": "LBP",
+            "COUNTRYNAME": "Malezya",
+            "COUNTRYCODE": "MY",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LBW",
+            "AIRPORTNAME": "Long Bawan (Long Bawan-LBW)",
+            "CITYNAME": "Long Bawan",
+            "CITYCODE": "LBW",
+            "COUNTRYNAME": "Endonezya",
+            "COUNTRYCODE": "ID",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LCP",
+            "AIRPORTNAME": "Loncopue (Loncopue-LCP)",
+            "CITYNAME": "Loncopue",
+            "CITYCODE": "LCP",
+            "COUNTRYNAME": "Arjantin",
+            "COUNTRYCODE": "AR",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LCX",
+            "AIRPORTNAME": "Longyan (Liancheng Apt-LCX)",
+            "CITYNAME": "Longyan",
+            "CITYCODE": "LCX",
+            "COUNTRYNAME": "Çin",
+            "COUNTRYCODE": "CN",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LDB",
+            "AIRPORTNAME": "Londrina (LDB)",
+            "CITYNAME": "Londrina",
+            "CITYCODE": "LDB",
+            "COUNTRYNAME": "Brezilya",
+            "COUNTRYCODE": "BR",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LDY",
+            "AIRPORTNAME": "Londonderry (LDY-City of Derry)",
+            "CITYNAME": "Londonderry",
+            "CITYCODE": "LDY",
+            "COUNTRYNAME": "Birleşik Krallık",
+            "COUNTRYCODE": "GB",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LDZ",
+            "AIRPORTNAME": "Londolozi (LDZ)",
+            "CITYNAME": "Londolozi",
+            "CITYCODE": "LDZ",
+            "COUNTRYNAME": "Güney Afrika",
+            "COUNTRYCODE": "ZA",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LGB",
+            "AIRPORTNAME": "Long Beach, Kaliforniya (LGB-Long Beach İdari Bölge)",
+            "CITYNAME": "Long Beach",
+            "CITYCODE": "LGB",
+            "COUNTRYNAME": "Birleşik Devletler",
+            "COUNTRYCODE": "US",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LGL",
+            "AIRPORTNAME": "Long Lellang (LGL)",
+            "CITYNAME": "Long Lellang",
+            "CITYCODE": "LGL",
+            "COUNTRYNAME": "Malezya",
+            "COUNTRYCODE": "MY",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LIJ",
+            "AIRPORTNAME": "Long Island (Long Island-LIJ)",
+            "CITYNAME": "Long Island",
+            "CITYCODE": "LIJ",
+            "COUNTRYNAME": "Birleşik Devletler",
+            "COUNTRYCODE": "US",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LKH",
+            "AIRPORTNAME": "Long Akah Havalimani (LKH)",
+            "CITYNAME": "Long Akah",
+            "CITYCODE": "LKH",
+            "COUNTRYNAME": "Malezya",
+            "COUNTRYCODE": "MY",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LLM",
+            "AIRPORTNAME": "Long Lama (Long Lama-LLM)",
+            "CITYNAME": "Long Lama",
+            "CITYCODE": "LLM",
+            "COUNTRYNAME": "Malezya",
+            "COUNTRYCODE": "MY",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        },
+        {
+            "AIRPORTCODE": "LNE",
+            "AIRPORTNAME": "Melsisi (LNE)",
+            "CITYNAME": "Lonorore",
+            "CITYCODE": "LNE",
+            "COUNTRYNAME": "Vanuatu",
+            "COUNTRYCODE": "VU",
+            "LOCATIONTYPE": "A",
+            "ORDERID": 1,
+            "RANK": 5,
+            "SEOURL": "",
+            "ISSEO": false
+        }
+    ]
+}
+
+// import data from "./fligthJSONList.json"
+
+// var response = data;
+
+// resp = response.response
+
+// Print to console hello world
+console.log("Hello World")
+
+// resp.sort((x,y) => x.CITYNAME.length - y.CITYNAME.length)
+
+// resp.sort((x,y) => x.AIRPORTNAME.length - y.AIRPORTNAME.length)
+
+// var strResp = JSON.stringify(resp)
+
+// localStorage.setItem("Test", strResp)
+
+// var test = localStorage.getItem("Test")
+
+// JSON.parse(test)
+
+// for(var i of resp){
+//     console.log(i.AIRPORTNAME)
+// }
